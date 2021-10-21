4b86f6ed93f71235f76cd01109c541d571bf8bd7
fwg <adf0001@163.com>
2021-10-21 13:35:44 +0800

    improve checking

--- is-simple-object.js
+++ is-simple-object.js
@@ -6,14 +6,10 @@ var is_simple_object = function (value) {
 	if (typeof value !== "object" || !value) return false;
 
 	var prototype = Object.getPrototypeOf(value);
+
 	return prototype === null ||
-		(
-			(
-				prototype.constructor === Object ||
-				!("constructor" in prototype)	//for inheriting from Object.create(null)
-			) &&
-			is_simple_object(prototype)
-		);
+		!("constructor" in prototype) ||		//for Object.create(null)
+		(prototype.constructor === Object && is_simple_object(prototype));
 }
 
 module.exports = is_simple_object;
